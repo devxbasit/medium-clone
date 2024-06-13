6f4d30fa33f9f685b626529c9ce8b77d7884dc3f
Basit <devxbasit@gmail.com>
2024-06-13 11:34:26 +0530

    Fixed HeaderComponent User Image

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, OnInit, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { User } from 'src/app/core/models/user.model';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userService = inject(UserService);
+  cdRef = inject(ChangeDetectorRef);
+  dRef = inject(DestroyRef);
+  currentUser?: User;
 
+  ngOnInit(): void {
+    this.userService.currentUser$.pipe(takeUntilDestroyed(this.dRef)).subscribe({
+      next: (currentUser: User) => {
+        this.currentUser = currentUser;
+        this.cdRef.markForCheck();
+      },
+      error: (error) => {
+        this.cdRef.markForCheck();
+      },
+    });
+  }
 }
